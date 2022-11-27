4de7e3701a8de0dcd5ccaed463fb93d9b6c3d398
danischm <danischm@cisco.com>
2022-11-27 07:55:19 +0100

    Add pre check

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,6 +1,9 @@
 package provider
 
 import (
+	"os"
+	"testing"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
@@ -14,3 +17,18 @@ var (
 		"iosxr": providerserver.NewProtocol6WithError(New()),
 	}
 )
+
+func testAccPreCheck(t *testing.T) {
+	// You can add code here to run prior to any test case execution, for example assertions
+	// about the appropriate environment variables being set are common to see in a pre-check
+	// function.
+	if v := os.Getenv("IOSXR_USERNAME"); v == "" {
+		t.Fatal("IOSXR_USERNAME env variable must be set for acceptance tests")
+	}
+	if v := os.Getenv("IOSXR_PASSWORD"); v == "" {
+		t.Fatal("IOSXR_PASSWORD env variable must be set for acceptance tests")
+	}
+	if v := os.Getenv("IOSXR_HOST"); v == "" {
+		t.Fatal("IOSXR_HOST env variable must be set for acceptance tests")
+	}
+}

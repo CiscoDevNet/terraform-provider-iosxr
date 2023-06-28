85d57f172ba0024aedb19bdbb6154a5186f8d78b
danischm <danischm@cisco.com>
2023-06-28 11:05:10 +0200

    Revert mutex change

--- internal/provider/client/client.go
+++ internal/provider/client/client.go
@@ -128,6 +128,7 @@ func (c *Client) Set(ctx context.Context, device string, operations ...SetOperat
 
 	var setResp *gnmi.SetResponse
 	for attempts := 0; ; attempts++ {
+		c.Devices[device].SetMutex.Lock()
 		err = target.CreateGNMIClient(ctx)
 		if err != nil {
 			diags.AddError(
@@ -136,12 +137,11 @@ func (c *Client) Set(ctx context.Context, device string, operations ...SetOperat
 			)
 			return nil, diags
 		}
-		c.Devices[device].SetMutex.Lock()
 		tflog.Debug(ctx, fmt.Sprintf("gNMI set request: %s", setReq.String()))
 		setResp, err = target.Set(ctx, setReq)
-		c.Devices[device].SetMutex.Unlock()
-		target.Close()
 		tflog.Debug(ctx, fmt.Sprintf("gNMI set response: %s", setResp.String()))
+		target.Close()
+		c.Devices[device].SetMutex.Unlock()
 		if err != nil {
 			if ok := c.Backoff(ctx, attempts); !ok {
 				diags.AddError("Client Error", fmt.Sprintf("Set request failed, got error: %s", err))

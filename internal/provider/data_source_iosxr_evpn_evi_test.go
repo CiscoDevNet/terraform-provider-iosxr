2373fcfed7b207360be5ec17caf15864ed0070bb
Daniel Schmidt <79086712+danischm@users.noreply.github.com>
2025-10-06 08:10:00 +0200

    Definition fixes (#296)

--- internal/provider/data_source_iosxr_evpn_evi_test.go
+++ internal/provider/data_source_iosxr_evpn_evi_test.go
@@ -41,8 +41,8 @@ func TestAccDataSourceIosxrEVPNEVI(t *testing.T) {
 	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_target_import_two_byte_as_format.0.assigned_number", "1"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_target_export_ipv4_address_format.0.ipv4_address", "1.1.1.1"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_target_export_ipv4_address_format.0.assigned_number", "1"))
-	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_policy_import", "ROUTE_POLICY_1"))
-	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_policy_export", "ROUTE_POLICY_1"))
+	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_policy_import", "EVI_POLICY_1"))
+	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "bgp_route_policy_export", "EVI_POLICY_1"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "advertise_mac", "true"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "unknown_unicast_suppression", "true"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.iosxr_evpn_evi.test", "control_word_disable", "true"))
@@ -66,10 +66,10 @@ func TestAccDataSourceIosxrEVPNEVI(t *testing.T) {
 // Section below is generated&owned by "gen/generator.go". //template:begin testPrerequisites
 const testAccDataSourceIosxrEVPNEVIPrerequisitesConfig = `
 resource "iosxr_gnmi" "PreReq0" {
-	path = "Cisco-IOS-XR-um-route-policy-cfg:/routing-policy/route-policies/route-policy[route-policy-name=ROUTE_POLICY_1]"
+	path = "Cisco-IOS-XR-um-route-policy-cfg:/routing-policy/route-policies/route-policy[route-policy-name=EVI_POLICY_1]"
 	attributes = {
-		"route-policy-name" = "ROUTE_POLICY_1"
-		"rpl-route-policy" = "route-policy ROUTE_POLICY_1\n  pass\nend-policy\n"
+		"route-policy-name" = "EVI_POLICY_1"
+		"rpl-route-policy" = "route-policy EVI_POLICY_1\n  pass\nend-policy\n"
 	}
 }
 
@@ -102,8 +102,8 @@ func testAccDataSourceIosxrEVPNEVIConfig() string {
 	config += `		ipv4_address = "1.1.1.1"` + "\n"
 	config += `		assigned_number = 1` + "\n"
 	config += `	}]` + "\n"
-	config += `	bgp_route_policy_import = "ROUTE_POLICY_1"` + "\n"
-	config += `	bgp_route_policy_export = "ROUTE_POLICY_1"` + "\n"
+	config += `	bgp_route_policy_import = "EVI_POLICY_1"` + "\n"
+	config += `	bgp_route_policy_export = "EVI_POLICY_1"` + "\n"
 	config += `	advertise_mac = true` + "\n"
 	config += `	unknown_unicast_suppression = true` + "\n"
 	config += `	control_word_disable = true` + "\n"
--- internal/provider/resource_iosxr_segment_routing_v6_test.go
+++ internal/provider/resource_iosxr_segment_routing_v6_test.go
@@ -44,9 +44,6 @@ func TestAccIosxrSegmentRoutingV6(t *testing.T) {
 	checks = append(checks, resource.TestCheckResourceAttr("iosxr_segment_routing_v6.test", "locators.0.prefix_length", "48"))
 	checks = append(checks, resource.TestCheckResourceAttr("iosxr_segment_routing_v6.test", "formats.0.name", "usid-f3216"))
 	checks = append(checks, resource.TestCheckResourceAttr("iosxr_segment_routing_v6.test", "formats.0.format_enable", "true"))
-	checks = append(checks, resource.TestCheckResourceAttr("iosxr_segment_routing_v6.test", "formats.0.usid_local_id_block_ranges_lib_start", "57344"))
-	checks = append(checks, resource.TestCheckResourceAttr("iosxr_segment_routing_v6.test", "formats.0.usid_local_id_block_ranges_explict_lib_start", "65024"))
-	checks = append(checks, resource.TestCheckResourceAttr("iosxr_segment_routing_v6.test", "formats.0.usid_wide_local_id_block_explicit_range", "65527"))
 	var steps []resource.TestStep
 	if os.Getenv("SKIP_MINIMUM_TEST") == "" {
 		steps = append(steps, resource.TestStep{
@@ -113,9 +110,6 @@ func testAccIosxrSegmentRoutingV6Config_all() string {
 	config += `	formats = [{` + "\n"
 	config += `		name = "usid-f3216"` + "\n"
 	config += `		format_enable = true` + "\n"
-	config += `		usid_local_id_block_ranges_lib_start = 57344` + "\n"
-	config += `		usid_local_id_block_ranges_explict_lib_start = 65024` + "\n"
-	config += `		usid_wide_local_id_block_explicit_range = 65527` + "\n"
 	config += `		}]` + "\n"
 	config += `}` + "\n"
 	return config

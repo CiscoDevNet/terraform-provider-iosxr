db8502e731ea1a7c455a6ba22a71148852cb971c
rocrowe <rocrowe@cisco.com>
2025-11-01 14:08:47 -0400

    iosxr_cli add

---
+++ internal/provider/resource_iosxr_cli_test.go
@@ -0,0 +1,47 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccIosxrCli(t *testing.T) {
+	var checks []resource.TestCheckFunc
+	checks = append(checks, resource.TestCheckResourceAttr("iosxr_cli.test", "cli", "interface Loopback0 description configured-via-gnmi-cli"))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIosxrCliConfig_basic(),
+				Check:  resource.ComposeTestCheckFunc(checks...),
+			},
+		},
+	})
+}
+
+func testAccIosxrCliConfig_basic() string {
+	config := `resource "iosxr_cli" "test" {` + "\n"
+	config += `	cli = "interface Loopback0 description configured-via-gnmi-cli"` + "\n"
+	config += `}` + "\n"
+	return config
+}
